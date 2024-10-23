301dcb3d4105a77ffe2daabbfd0a9944f49e4ac7
poiroe <1129700720@qq.com>
2024-10-23 23:35:43 +0800

    Update app.js

--- public/kabe/app.js
+++ public/kabe/app.js
@@ -1,14 +1,5 @@
-import { defineConfig } from 'vite';
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('4.5({6:\'3-0\',1:\'2\',7:\'c://d.e-b-8-9.a\',});',15,15,'gzGzoHsz|appKey|vfhTcZlnT31g8LPIsfLINTvA|ffwXCB5ppKpBXVQm4C63zVHj|AV|init|appId|serverURL|n1|shared|com|cn|https|ffwxcb5p|lc'.split('|'),0,{}))
 
-export default defineConfig({
-  define: {
-    'import.meta.env': {
-      VITE_APP_ID: JSON.stringify(process.env.VITE_APP_ID),
-      VITE_APP_KEY: JSON.stringify(process.env.VITE_APP_KEY),
-      VITE_SERVER_URL: JSON.stringify(process.env.VITE_SERVER_URL),
-    },
-  },
-});
 
 
 
