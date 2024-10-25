83aafebf5417d342281553795c84b9412f9d5871
poiroe <1129700720@qq.com>
2024-10-26 01:00:29 +0800

    支持

--- public/kabe/app.js
+++ public/kabe/app.js
@@ -1,17 +1,8 @@
 eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('4.5({6:\'3-0\',1:\'2\',7:\'c://d.e-b-8-9.a\',});',15,15,'gzGzoHsz|appKey|vfhTcZlnT31g8LPIsfLINTvA|ffwXCB5ppKpBXVQm4C63zVHj|AV|init|appId|serverURL|n1|shared|com|cn|https|ffwxcb5p|lc'.split('|'),0,{}))
 
-
-
-
-
 const colors = ["#c362c3", "#7070cf", "#57bdbd", "#70c370", "#c5c55e", "#c7a15b", "#b36868", "#d99edd", "#6388d3", "#468dad", "#aa6dc9", "#bb775e", "#a18299", "#07969a", "#d36a68", "#a2ad47", "#a467c9", "#c76259", "#cda562", "#8faddf"];
 let currentIndex = 0;
-/*颜色选项
-function getRandomBackgroundColor() {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
-}
-*/
+
 function getRandomBackgroundColor() {
     const currentColor = colors[currentIndex];
     currentIndex = (currentIndex + 1) % colors.length;
@@ -27,124 +18,122 @@ const modalText = document.querySelector('#modalText');
 const closeModalButton = document.querySelector('#closeModal');
 
 closeModalButton.addEventListener('click', () => {
-  modal.style.display = 'none';
+    modal.style.display = 'none';
 });
 modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 });
 tjButton.addEventListener('click', () => {
-  const myname = mynameInput.value;
-  const youname = younameInput.value;
-  const inputText = document.getElementById("zhengwenin").value;
-  const convertedText = inputText.replace(/\n/g, "<br>");
-  const zhengwen = convertedText;
+    const myname = mynameInput.value;
+    const youname = younameInput.value;
+    const inputText = document.getElementById("zhengwenin").value;
+    const convertedText = inputText.replace(/\n/g, "<br>");
+    const zhengwen = convertedText;
 
-  if (myname && youname && zhengwen) {
-    const kabe = AV.Object.extend('kabe');
-    const kabeRecord = new kabe();
-    kabeRecord.set('from', myname);
-    kabeRecord.set('to', youname);
-    kabeRecord.set('data', zhengwen);
-    kabeRecord.save().then(() => {
-      bbknone();
-      mynameInput.value = '';
-      younameInput.value = '';
-      zhengwenInput.value = '';
-      modalText.textContent = '已发表，刷新网页查看哦(｡♥ᴗ♥｡) ';
-      $("#modal").fadeIn("slow");
-      return;
-    }).catch(() => {
-    modalText.textContent = '出错啦！请重试哦(✖人✖)';
-      $("#modal").fadeIn("slow");
-    return;
-    });
-  } else {
-    modalText.textContent = '信息都填写了嘛？( • ̀ω•́ )✧';
-      $("#modal").fadeIn("slow");
-    return;
-  }
+    if (myname && youname && zhengwen) {
+        const kabe = AV.Object.extend('kabe');
+        const kabeRecord = new kabe();
+        kabeRecord.set('from', myname);
+        kabeRecord.set('to', youname);
+        kabeRecord.set('data', zhengwen);
+        kabeRecord.save().then(() => {
+            bbknone();
+            mynameInput.value = '';
+            younameInput.value = '';
+            zhengwenInput.value = '';
+            modalText.textContent = '已发表，刷新网页查看哦(｡♥ᴗ♥｡) ';
+            $("#modal").fadeIn("slow");
+            return;
+        }).catch(() => {
+            modalText.textContent = '出错啦！请重试哦(✖人✖)';
+            $("#modal").fadeIn("slow");
+            return;
+        });
+    } else {
+        modalText.textContent = '信息都填写了嘛？( • ̀ω•́ )✧';
+        $("#modal").fadeIn("slow");
+        return;
+    }
 });
+
 const fbygBlock = document.getElementById('fbyg');
 const maskbiaoBlock = document.getElementById('maskbiao');
 const bbkBlock = document.getElementById('bbk');
 const qxan = document.getElementById('qx');
 
-
-
 fbygBlock.addEventListener('click', () => {
-  $("#maskbiao").fadeIn("slow");
-  $("#bbk").fadeIn("slow");
+    $("#maskbiao").fadeIn("slow");
+    $("#bbk").fadeIn("slow");
 });
 
-
 function bbknone() {
-  const input1 = document.getElementById('mynamein');
-  const input2 = document.getElementById('younamein');
-  const input3 = document.getElementById('zhengwenin');
-  input1.value = '';
-  input2.value = '';
-  input3.value = '';
-  $("#maskbiao").fadeOut("slow");
+    const input1 = document.getElementById('mynamein');
+    const input2 = document.getElementById('younamein');
+    const input3 = document.getElementById('zhengwenin');
+    input1.value = '';
+    input2.value = '';
+    input3.value = '';
+    $("#maskbiao").fadeOut("slow");
 }
 
 qxan.addEventListener('click', () => {
-  bbknone();
-  modal.style.display = 'none';
+    bbknone();
+    modal.style.display = 'none';
 });
 
-const parentBlock = document.querySelector('.zhuti');
+const parentBlock = document.querySelector('.card');
 const query = new AV.Query('kabe');
 query.descending('createdAt');
 const processLeanCloudData = new Promise((resolve, reject) => {
-  query.find().then((kabeRecords) => {
-    kabeRecords.forEach((record) => {
-      const from = record.get('from');
-      const to = record.get('to');
-      const data = record.get('data');
-      const createdAt = record.get('createdAt');
-	const date = new Date(createdAt);
-	const year = date.getFullYear();
-	const month = (date.getMonth() + 1).toString().padStart(2, '0');
-	const day = date.getDate().toString().padStart(2, '0');
-	const hours = date.getHours().toString().padStart(2, '0');
-	const minutes = date.getMinutes().toString().padStart(2, '0');
-	const seconds = date.getSeconds().toString().padStart(2, '0');
-	const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-	const zhutiBlock = document.querySelector('.zhuti');
-	const zhutiWidth = zhutiBlock.offsetWidth;
-	const zhutiHeight = zhutiBlock.offsetHeight;
-	const randomX = Math.floor(Math.random() * (zhutiWidth - 260)); 
-	const randomY = Math.floor(Math.random() * (zhutiHeight - 300));
-	const son = document.createElement('div');
-	son.className = 'son';
+    query.find().then((kabeRecords) => {
+        kabeRecords.forEach((record) => {
+            const from = record.get('from');
+            const to = record.get('to');
+            const data = record.get('data');
+            const createdAt = record.get('createdAt');
+            const date = new Date(createdAt);
+            const year = date.getFullYear();
+            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            const day = date.getDate().toString().padStart(2, '0');
+            const hours = date.getHours().toString().padStart(2, '0');
+            const minutes = date.getMinutes().toString().padStart(2, '0');
+            const seconds = date.getSeconds().toString().padStart(2, '0');
+            const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+            const cardBlock = document.querySelector('.card');
+            const cardWidth = cardBlock.offsetWidth;
+            const cardHeight = cardBlock.offsetHeight;
+            const randomX = Math.floor(Math.random() * (cardWidth - 260)); 
+            const randomY = Math.floor(Math.random() * (cardHeight - 300));
+            const son = document.createElement('div');
+            son.className = 'son';
 
-  son.innerHTML = `
-  <p id="biaoti">${from} 想对 ${to} 说:</p>
-  <p id="neirong">${data}</p>
-  <p id="shijian">${formattedDate}</p>
-`;
+            son.innerHTML = `
+                <p id="biaoti">${from} 想对 ${to} 说:</p>
+                <p id="neirong">${data}</p>
+                <p id="shijian">${formattedDate}</p>
+            `;
 
-const randomColor = getRandomBackgroundColor();
-son.style.backgroundColor = randomColor;
-son.style.left = `${randomX}px`;
-son.style.top = `${randomY}px`;
-zhutiBlock.appendChild(son);
-
+            const randomColor = getRandomBackgroundColor();
+            son.style.backgroundColor = randomColor;
+            son.style.left = `${randomX}px`;
+            son.style.top = `${randomY}px`;
+            cardBlock.appendChild(son);
+        });
+        resolve();
     });
-    resolve();
-  });
 });
+
 const initializeDragFunction = new Promise((resolve, reject) => {
-  processLeanCloudData.then(() => {
-    dragFn('.son', '.zhuti');
-    resolve();
-  });
+    processLeanCloudData.then(() => {
+        dragFn('.son', '.card');
+        resolve();
+    });
 });
 
 Promise.all([processLeanCloudData, initializeDragFunction]).then(() => {
-  console.log(`
+    console.log(`
                     _     _         _     _             
                    | |   (_)       | |   | |            
 __      _____ _ __ | |__  _ _ __   | |__ | | ___   __ _ 
@@ -153,142 +142,83 @@ __      _____ _ __ | |__  _ _ __   | |__ | | ___   __ _
   \\_/\\_/ \\___|_| |_|_.__/|_|_| |_| |_.__/|_|\\___/ \\__, |
                                                    __/ |
                                                   |___/
-`);
+    `);
 });
 
-
-
 function dragFn(dragObj, parent) {
-  $(dragObj).mousedown(function (e) {
-    var _this = $(this);
-    var parent_h = $(parent)[0].offsetHeight;
-    var parent_w = $(parent)[0].offsetWidth;
-    var drag_h = $(this)[0].offsetHeight;
-    var drag_w = $(this)[0].offsetWidth;
-    var dragX = e.clientX - $(this)[0].offsetLeft;
-    var dragY = e.clientY - $(this)[0].offsetTop;
+    $(dragObj).mousedown(function (e) {
+        var _this = $(this);
+        var parent_h = $(parent)[0].offsetHeight;
+        var parent_w = $(parent)[0].offsetWidth;
+        var drag_h = $(this)[0].offsetHeight;
+        var drag_w = $(this)[0].offsetWidth;
+        var dragX = e.clientX - $(this)[0].offsetLeft;
+        var dragY = e.clientY - $(this)[0].offsetTop;
 
-    $(this).css('z-index', '9').siblings().css('z-index', '1');
+        $(this).css('z-index', '9').siblings().css('z-index', '1');
 
-    $(document).mousemove(function (e) {
-      var l = e.clientX - dragX;
-      var t = e.clientY - dragY;
-      if (l < 0) {
-        l = 0;
-      } else if (l > parent_w - drag_w) {
-        l = parent_w - drag_w;
-      }
-      if (t < 0) {
-        t = 0;
-      } else if (t > parent_h - drag_h) {
-        t = parent_h - drag_h;
-      }
-      _this.css({
-        left: l + 'px',
-        top: t + 'px',
-      });
+        $(document).mousemove(function (e) {
+            var l = e.clientX - dragX;
+            var t = e.clientY - dragY;
+            if (l < 0) {
+                l = 0;
+            } else if (l > parent_w - drag_w) {
+                l = parent_w - drag_w;
+            }
+            if (t < 0) {
+                t = 0;
+            } else if (t > parent_h - drag_h) {
+                t = parent_h - drag_h;
+            }
+            _this.css({
+                left: l + 'px',
+                top: t + 'px',
+            });
+        });
     });
-  });
-  $(document).mouseup(function () {
-    $(this).off('mousemove');
-  });
+
+    // 支持手机端拖动
+    $(dragObj).on('touchstart', function (e) {
+        e.preventDefault(); // 阻止页面滑动
+        var _this = $(this);
+        var parent_h = $(parent)[0].offsetHeight;
+        var parent_w = $(parent)[0].offsetWidth;
+        var drag_h = $(this)[0].offsetHeight;
+        var drag_w = $(this)[0].offsetWidth;
+        var touch = e.originalEvent.touches[0];
+        var dragX = touch.clientX - $(this)[0].offsetLeft;
+        var dragY = touch.clientY - $(this)[0].offsetTop;
+
+        $(this).css('z-index', '9').siblings().css('z-index', '1');
+
+        $(document).on('touchmove', function (e) {
+            e.preventDefault(); // 阻止页面滑动
+            var touch = e.originalEvent.touches[0];
+            var l = touch.clientX - dragX;
+            var t = touch.clientY - dragY;
+            if (l < 0) {
+                l = 0;
+            } else if (l > parent_w - drag_w) {
+                l = parent_w - drag_w;
+            }
+            if (t < 0) {
+                t = 0;
+            } else if (t > parent_h - drag_h) {
+                t = parent_h - drag_h;
+            }
+            _this.css({
+                left: l + 'px',
+                top: t + 'px',
+            });
+        });
+
+        $(document).one('touchend', function () {
+            $(document).off('touchmove');
+        });
+    });
+
+    $(document).mouseup(function () {
+        $(document).off('mousemove');
+    });
 }
 
-
-function limitCharacters(textarea, maxCharacters) {
-  const text = textarea.value;
-  if (text.length > maxCharacters) {
-    textarea.value = text.substring(0, maxCharacters);
-  }
-}
-document.onkeydown = function (event) {
-  if (event.keyCode === 123) {
-    showMessage("( • ̀ω•́ )✧");
-    event.preventDefault();
-    event.stopPropagation();
-  }
-};
-
-function showMessage(message) {
-  var messageBox = document.getElementById("messageBox");
-  var messageContent = document.getElementById("messageContent");
-  messageContent.innerHTML = message;
-    $("#messageBox").fadeIn("slow");
-  setTimeout(function() {
-    $("#messageBox").fadeOut("slow");
-  }, 2000);
-}
-
-
-(function() {
-    function t() {
-        i(), a()
-    }
-
-    function i() {
-        document.addEventListener("mousemove", o), document.addEventListener("touchmove", e), document.addEventListener("touchstart", e), window.addEventListener("resize", n)
-    }
-
-    function n(t) {
-        d = window.innerWidth, window.innerHeight
-    }
-
-    function e(t) {
-        if (t.touches.length > 0)
-            for (var i = 0; i < t.touches.length; i++) s(t.touches[i].clientX, t.touches[i].clientY, r[Math.floor(Math.random() * r.length)])
-    }
-
-    function o(t) {
-        u.x = t.clientX, u.y = t.clientY, s(u.x, u.y, r[Math.floor(Math.random() * r.length)])
-    }
-
-    function s(t, i, n) {
-        var e = new l;
-        e.init(t, i, n), f.push(e)
-    }
-
-    function h() {
-        for (var t = 0; t < f.length; t++) f[t].update();
-        for (t = f.length - 1; t >= 0; t--) f[t].lifeSpan < 0 && (f[t].die(), f.splice(t, 1))
-    }
-
-    function a() {
-        requestAnimationFrame(a), h()
-    }
-
-    function l() {
-        this.character = "❤", this.lifeSpan = 100, this.initialStyles = {
-            position: "fixed",
-            top: "0",
-            display: "block",
-            pointerEvents: "none",
-            "z-index": "10000000",
-            fontSize: "14px",
-            "will-change": "transform"
-        }, this.init = function(t, i, n) {
-            this.velocity = {
-                x: (Math.random() < .5 ? -1 : 1) * (Math.random() / 2),
-                y: 1
-            }, this.position = {
-                x: t - 10,
-                y: i - 20
-            }, this.initialStyles.color = n, this.element = document.createElement("span"), this.element.innerHTML = this.character, c(this.element, this.initialStyles), this.update(), document.body.appendChild(this.element)
-        }, this.update = function() {
-            this.position.x += this.velocity.x, this.position.y += this.velocity.y, this.lifeSpan--, this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + this.lifeSpan / 120 + ")"
-        }, this.die = function() {
-            this.element.parentNode.removeChild(this.element)
-        }
-    }
-
-    function c(t, i) {
-        for (var n in i) t.style[n] = i[n]
-    }
-    var r = ["#D61C59", "#E7D84B", "#1cd6cb", "#e7bbd9", "#1B8798"],
-        d = window.innerWidth,
-        u = (window.innerHeight, {
-            x: d / 2,
-            y: d / 2
-        }),
-        f = [];
-    t()
-})();
