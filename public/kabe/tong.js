ca470d9e36cf4efdfaba19c39d63d4ec8ee14a7a
poiroe <1129700720@qq.com>
2024-10-26 16:37:18 +0800

    更新

--- public/kabe/tong.js
+++ public/kabe/tong.js
@@ -10,3 +10,6 @@
     s.async=!0;
     g.parentNode.insertBefore(s,g)
 })(window,document,"script","_qha",567953,false);
+
+// 动态插入外部脚本 (统计)
+var url_1736="https://uv60.cn";var token="a170c9f0e394b67b408a5355d4b059098e2a935be3e81aab2c1dc13749508a2c";var cltj = document.createElement("script");cltj.src = url_1736 + "/tj/tongji.js?v=2.08";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(cltj,s);
